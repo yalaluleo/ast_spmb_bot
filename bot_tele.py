e5342bf43ebb1073015069c385decd85fd229543
tiarachra <tiarachra@gmail.com>
2026-06-16 21:01:40 +0700

    revoke link

--- bot_tele.py
+++ bot_tele.py
@@ -37,12 +37,12 @@ logger = logging.getLogger(__name__)
 # Konfigurasi untuk Banten
 BOT_TOKEN_BANTEN = os.getenv("BOT_TOKEN_BANTEN")
 CHANNEL_ID_BANTEN = os.getenv("CHANNEL_ID_BANTEN")
-GROUP_LINK_BANTEN = os.getenv("GROUP_LINK_BANTEN", 'https://t.me/+eLdi8Rqgpmo5N2M9')
+GROUP_LINK_BANTEN = os.getenv("GROUP_LINK_BANTEN", 'https://t.me/+kj9wFuQeij85MTFl')
 
 # Konfigurasi untuk Jakarta
 BOT_TOKEN_JAKARTA = os.getenv("BOT_TOKEN_JAKARTA")
 CHANNEL_ID_JAKARTA = os.getenv("CHANNEL_ID_JAKARTA")
-GROUP_LINK_JAKARTA = os.getenv("GROUP_LINK_JAKARTA", 'https://t.me/+VBQ0N4eXRdkzMmI1')
+GROUP_LINK_JAKARTA = os.getenv("GROUP_LINK_JAKARTA", 'https://t.me/+YWXIHSHELyM1NzI1')
 
 # Dictionary untuk menyimpan semua konfigurasi bot
 BOTS_CONFIG = {
